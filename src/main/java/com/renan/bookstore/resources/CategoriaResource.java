e98603e982beb2c1feb3ef9d0c669d3dca5e284e
renanmaziero <renanfmaziero@gmail.com>
2021-09-11 23:31:07 -0300

    UPDATE para Categoria

--- src/main/java/com/renan/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/renan/bookstore/resources/CategoriaResource.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,7 @@ public class CategoriaResource {
 
 	}
 
+	// select
 	@GetMapping
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> list = service.findAll();
@@ -39,11 +41,20 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(listDTO);
 	}
 
+	// insert
 	@PostMapping
 	public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
 		obj = service.create(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+
+	// update
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoriaDTO> update(@PathVariable Integer id, @RequestBody CategoriaDTO objDto) {
+		Categoria newObj = service.update(id, objDto);
+		return ResponseEntity.ok().body(new CategoriaDTO(newObj));
+
 	}
 }
 
--- src/main/java/com/renan/bookstore/service/CategoriaService.java
+++ src/main/java/com/renan/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.renan.bookstore.domain.Categoria;
+import com.renan.bookstore.dtos.CategoriaDTO;
 import com.renan.bookstore.repositories.CategoriaRepository;
 import com.renan.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -29,6 +30,13 @@ public class CategoriaService {
 
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
 	}
 }
